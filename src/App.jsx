4436e28d7d81f04cd22d76a72679c4ab7e137094
bechagas <bechagas.chagas@gmail.com>
2026-05-29 10:19:46 -0300

    feat(rotas): configurar roteamento da página Sobre com react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Sobre from './pages/sobre/Sobre';
 
 const App = () => {
   return (
-    <Sobre />
+    <BrowserRouter>
+      <Routes>
+        {/* Rota principal e fallback direcionadas para a página Sobre */}
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="*" element={<Sobre />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
 export default App;
+
