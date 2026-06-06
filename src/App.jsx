ebdd6e1e4dd95c6d07441cc1ea8518048290870b
Pedro Lucas <pedrolucasfonseca98@gmail.com>
2026-06-06 16:15:33 -0300

    chore: adiciona a homepage ao app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
 import Sobre from './pages/sobre/Sobre';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -8,8 +9,9 @@ const App = () => {
     <>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/sobre" element={<Sobre />} />
-        <Route path="*" element={<Sobre />} />
+        <Route path="*" element={<Home />} />
       </Routes>
       <Footer />
     </>
